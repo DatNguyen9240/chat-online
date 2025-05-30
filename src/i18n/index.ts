1fbcba0c606bb16867e1b64425a545335a4f38db
Nguyễn Thành Đạt <datnguyen9240@gmail.com>
2025-05-30 21:59:51 +0700

    fix: remove language detector

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import conversation_en from "./locales/en/conversations.json";
 import conversation_vi from "./locales/vi/conversations.json";
 
@@ -16,19 +14,15 @@ export const resources = {
 
 export const lang = [{ vi: "🇻🇳 Tiếng Việt" }, { en: "🇬🇧 English" }];
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources,
-    fallbackLng: "en",
-    lng: "en",
-    defaultNS: "conversations",
-    ns: ["conversations"],
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: "en",
+  lng: "en",
+  defaultNS: "conversations",
+  ns: ["conversations"],
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
